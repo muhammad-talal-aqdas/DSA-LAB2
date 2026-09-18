8c824aba5e2b1132c11f788bd732aab83793e862
muhammad-talal-aqdas <maqdas.bese25seecs@seecs.edu.pk>
2026-09-18 15:48:29 +0500

    Implement isSorted function with unit tests

---
+++ is_sorted.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+bool isSorted(const int arr[], const int size);
+
+void testSortedArray() {
+    int arr[] = {1, 2, 3, 4, 5};
+    assert(isSorted(arr, 5) == true);
+}
+
+void testUnsortedArray() {
+    int arr[] = {1, 4, 3, 5, 6};
+    assert(isSorted(arr, 5) == false);
+}
+
+void testDuplicateValues() {
+    int arr[] = {1, 2, 2, 3, 5};
+    assert(isSorted(arr, 5) == true);
+}
+
+void testSingleElement() {
+    int arr[] = {10};
+    assert(isSorted(arr, 1) == true);
+}
+
+void testDescendingArray() {
+    int arr[] = {5, 4, 3, 2, 1};
+    assert(isSorted(arr, 5) == false);
+}
+
+void testNegativeValues() {
+    int arr[] = {-10, -5, 0, 5, 10};
+    assert(isSorted(arr, 5) == true);
+}
+
+bool isSorted(const int arr[], const int size) {
+    if (size <= 1) return true;
+
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    testSortedArray();
+    testUnsortedArray();
+    testDuplicateValues();
+    testSingleElement();
+    testDescendingArray();
+    testNegativeValues();
+
+    cout << "All TDD unit tests passed successfully!" << endl;
+    return 0;
+}
